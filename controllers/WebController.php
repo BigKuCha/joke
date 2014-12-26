72ff208f15af70f46742ac9e462d880d7c83b318
root <root@localhost.localdomain>
2014-12-26 23:04:08 +0800

    init

---
+++ controllers/WebController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *	  ┏┓　　　┏┓
+ *	┏┛┻━━━┛┻┓
+ *	┃　　　　　　　┃
+ *	┃　　　━　　　┃
+ *	┃　┳┛　┗┳　┃
+ *	┃　　　　　　　┃
+ *	┃　　　┻　　　┃
+ *	┃　　　　　　　┃
+ *	┗━┓　　　┏━┛
+ *	    ┃　　　┃   神兽保佑
+ *	    ┃　　　┃   代码无BUG！
+ *	 	 ┃　　　┗━━━┓
+ *	    ┃　　　　　　　┣┓
+ *	    ┃　　　　　　　┏┛
+ *	    ┗┓┓┏━┳┓┏┛
+ *	      ┃┫┫　┃┫┫
+ *	      ┗┻┛　┗┻┛
+ */ 
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class WebController extends Controller
+{
+
+}
